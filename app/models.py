62817d3eb965f9f68a5f423dbf69f2999ede1e6d
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-04-30 15:38:16 +0530

    feat: :sparkles: adjusted models and removed all cache

--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class User(AbstractUser):
     emailPass = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}" or self.email or self.username
+
 
 class Placeholder(models.Model):
     kind = models.CharField(
@@ -16,11 +19,18 @@ class Placeholder(models.Model):
     template = models.ForeignKey("CertificateTemplate", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.name} in {self.template} @({self.x},{self.y})"
+
 
 class CertificateTemplate(models.Model):
     image = models.ImageField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} by {self.user}"
 
 
 class CertificateSession(models.Model):
@@ -28,6 +38,9 @@ class CertificateSession(models.Model):
     template = models.ForeignKey(CertificateTemplate, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     recievers = models.ManyToManyField(User, related_name="recievers")
+
+    def __str__(self):
+        return f"{self.template} for {self.recievers.count()}"
 
 
 class reciever(models.Model):
@@ -38,8 +51,14 @@ class reciever(models.Model):
     class Meta:
         unique_together = (("name", "email"),)
 
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+
 
 class recieved_certificate(models.Model):
     id = models.UUIDField(primary_key=True)
     reciever = models.ForeignKey(reciever, on_delete=models.CASCADE)
     template = models.ForeignKey(CertificateTemplate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.reciever} recieved {self.template}"
